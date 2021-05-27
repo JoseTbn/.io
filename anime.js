9c3193040631613e332782c183a49c0061d2ede4
JoseTbn <82665551+JoseTbn@users.noreply.github.com>
2021-05-27 06:41:34 +0200

    commit 2

--- anime.js
+++ anime.js
@@ -1,4 +1,4 @@
-const sourceUrl = "https://api.jikan.moe/v3/anime"
+const sourceUrl = "https://api.jikan.moe/v3"
 
 
 
@@ -6,38 +6,62 @@ const sourceUrl = "https://api.jikan.moe/v3/anime"
 
 
 
-function MangaSearch(event) {
+function mangaSearch(event) {
 
-    event.preventDefault;
+    event.preventDefault();
 
-const form = new FormData(this);
+const form = new FormData(search_form);
 const query = form.get("manga_name");
 
 console.log(query);
 
 
-    fetch(`${sourceUrl}//?q=${query}&page=1`, {
-        headers: {
-        Accept: 'application/json'
-        }
-    }) 
-    .then(response => { response.json()
-    .then(updateDom)
+    fetch(`${sourceUrl}/search/anime?q=${query}&page=1`)
+    .then(response =>  response.json())
+    .then(resultsData)
     .catch(err=>console.warn(err.message))
 
-})
+}
 
-function updateDom(data) {
-    data.results.forEach(anime => {
-        console.log(anime);
+function resultsData(data) {
+
+const  postResults = document.getElementById('postResults')
+
+    
+
+postResults.innerHTML = data.results.sort((a,b) =>a.episodes-b.episodes).map(anime => {
+        return `  <div class="card">
+        <div class="card-image waves-effect waves-block waves-light">
+        <img class="activator" src="${anime.image_url}">
+        </div>
+        <div class="card-content">
+        <span class="card-title activator grey-text text-darken-4">${anime.title}<i class="material-icons right">Read</i></span>
+        <p><a href="${anime.url}">myanimelist.net</a></p>
+        </div>
+        <div class="card-reveal">
+        <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+        <p>${anime.synopsis}</p>
+        <div> <img src="${anime.image_url}" alt=""> </div>
+        </div>
+    </div>
+        `
     });
     
-}
+};
 
 
 function pageLoaded(){ 
-const form = document.getElementById("search_name");
-form.addEventListener("submit", MangaSearch )
+const form = document.getElementById('search_form');
+form.addEventListener("submit", mangaSearch);
 }
 
-window.addEventListener("load", pageLoaded)};
+window.addEventListener("load", pageLoaded);
+
+
+
+
+/* fetch('https://api.jikan.moe/v3/anime/search/anime?q=${query}&page=1',{
+        headers: {
+        Accept: 'application/json'
+        }
+    })*/ 
